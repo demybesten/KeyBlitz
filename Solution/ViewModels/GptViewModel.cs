8e34b7719749c7e2589e4d9573443a075f387831
Demy <demybesten@outlook.com>
2023-11-21 21:50:55 +0100

    custom texts met subject, length en complexity

--- Solution/ViewModels/GptViewModel.cs
+++ Solution/ViewModels/GptViewModel.cs
@@ -15,21 +15,62 @@ namespace Solution.ViewModels
 {
   public class GptViewModel : INotifyPropertyChanged
   {
-    private string _responseText;
-    public string ResponseText
-    {
-        get { return _responseText; }
-        set
-        {
-            if (_responseText != value)
-            {
-                _responseText = value;
-                OnPropertyChanged();
-            }
-        }
-    }
+      private string _responseText;
+      public string ResponseText
+      {
+          get { return _responseText; }
+          set
+          {
+              if (_responseText != value)
+              {
+                  _responseText = value;
+                  OnPropertyChanged();
+              }
+          }
+      }
 
-    public ICommand SendPromptCommand { get; }
+      private string _subject;
+      public string Subject
+      {
+          get { return _subject; }
+          set
+          {
+              if (_subject != value)
+              {
+                  _subject = value;
+                  OnPropertyChanged();
+              }
+          }
+      }
+
+      private int _length;
+      public int Length
+      {
+          get { return _length; }
+          set
+          {
+              if (_length != value)
+              {
+                  _length = value;
+                  OnPropertyChanged();
+              }
+          }
+      }
+
+      private int _complexity;
+      public int Complexity
+      {
+          get { return _complexity; }
+          set
+          {
+              if (_complexity != value)
+              {
+                  _complexity = value;
+                  OnPropertyChanged();
+              }
+          }
+      }
+    public RelayCommand SendPromptCommand { get; }
 
     public GptViewModel()
     {
@@ -38,6 +79,36 @@ namespace Solution.ViewModels
 
     public async Task SendPrompt()
     {
+        string userMessage = "a short text about baby seals";
+        switch (Complexity)
+        {
+            case 1:
+                userMessage = $"Generate a text on the subject of '{Subject}'" +
+                                     $" that is {Length} words long with Simple sentences and phrases and common and frequently used words also don't use any special characters";
+                break;
+            
+           case 2:
+                userMessage = $"Generate a text on the subject of '{Subject}'" +
+                                    $" that is {Length} words long with Expanded vocabulary with common words and basic sentence structures with some variety ";
+                break;
+           
+           case 3:
+               userMessage = $"Generate a text on the subject of '{Subject}'" +
+                             $" that is {Length} words long with More varied sentence structures and a mix of common and less common words ";
+               break;
+           
+            case 4:
+                userMessage = $"Generate a text on the subject of '{Subject}'" +
+                              $" that is {Length} words long with Complex sentence structures and advanced vocabulary and varied word choices";
+                break;
+            case 5:
+                userMessage = $"Generate a text on the subject of '{Subject}'" +
+                              $" that is {Length} words long with advanced and nuanced language and rich vocabulary with technical or specialized terms";
+                break;
+            
+        }
+
+        
       // Declare the API key
       var apiKey = "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";
 
@@ -50,9 +121,9 @@ namespace Solution.ViewModels
       (new ChatCompletionCreateRequest()
       {
         Messages = new List<ChatMessage>(new ChatMessage[]
-          { new ChatMessage("user", "Generate a text that is 5 words long dont include any special characters") }),
+          { new ChatMessage("user", userMessage) }),
         Model = Models.ChatGpt3_5Turbo,
-        Temperature = 0.5F,
+        Temperature = 0.1F,
         MaxTokens = 100,
         N = 1
       });
