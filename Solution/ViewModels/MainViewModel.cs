bc216c7a5a6d52e9e5d671ea877e18835ab7e41a
Daan Scholten <daanscholten04@gmail.com>
2023-11-21 13:11:15 +0100

    Add folders

---
+++ Solution/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged {
+        private object _currentViewModel;
+        public object CurrentViewModel {
+            get {
+                return _currentViewModel;
+            }
+            set {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Solution/Views/NewTestView.xaml.cs
+++ Solution/Views/NewTestView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Solution.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Solution {
-    public partial class MainWindow : Window {
-        public MainWindow() {
+namespace Solution.Views
+{
+    public partial class NewTestView : Window
+    {
+        public NewTestView()
+        {
             InitializeComponent();
         }
     }
