6ffcd28f3859dfa266b0ffcb44899bb5bb0f050d
Daan Scholten <daanscholten04@gmail.com>
2023-11-23 20:34:43 +0100

    Combobox style gefixt: alleen kleur van hoveren over item veranderd niet goed

--- Solution/ViewModels/NewTestViewModel.cs
+++ Solution/ViewModels/NewTestViewModel.cs
@@ -9,6 +9,20 @@ using System.Threading.Tasks;
 namespace Solution.ViewModels
 {
     public class NewTestViewModel : BaseViewModel{
+        public NewTestViewModel() {
+            _textLength = 20;
+            ComplexityLevels.Add("basic");
+            ComplexityLevels.Add("average");
+            ComplexityLevels.Add("advanced");
+            TextTypes.Add("story");
+            TextTypes.Add("sentences");
+            TextTypes.Add("words");
+            Languages.Add("english");
+            Languages.Add("dutch");
+            Languages.Add("german");
+            Languages.Add("french");
+        }
+
         private int _textLength;
         public int TextLength {
             get {
@@ -19,13 +33,7 @@ namespace Solution.ViewModels
                 OnPropertyChanged(nameof(TextLength));
             }
         }
-        public NewTestViewModel() {
-            _textLength=20;
-            ComplexityLevels.Add("basic");
-            ComplexityLevels.Add("avarage");
-            ComplexityLevels.Add("pro");
-        }
-
+        
         private ObservableCollection<string> _complexityLevels = new ObservableCollection<string>();
         public ObservableCollection<string> ComplexityLevels {
             get {
@@ -34,6 +42,61 @@ namespace Solution.ViewModels
             set {
                 _complexityLevels = value;
                 OnPropertyChanged(nameof(ComplexityLevels));
+            }
+        }
+
+        private ObservableCollection<string> _textTypes = new ObservableCollection<string>();
+        public ObservableCollection<string> TextTypes {
+            get {
+                return _textTypes;
+            }
+            set {
+                _textTypes = value;
+                OnPropertyChanged(nameof(TextTypes));
+            }
+        }
+
+        private string _textType = "story";
+        public string TextType {
+            get {
+                return _textType;
+            }
+            set {
+                _textType = value;
+                OnPropertyChanged(nameof(TextType));
+            }
+        }
+
+        private ObservableCollection<string> _languages = new ObservableCollection<string>();
+        public ObservableCollection<string> Languages {
+            get {
+                return _languages;
+            }
+            set {
+                _languages = value;
+                OnPropertyChanged(nameof(Languages));
+            }
+        }
+
+        private string _language = "english";
+        public string Language {
+            get {
+                return _language;
+            }
+            set {
+                _language = value;
+                OnPropertyChanged(nameof(Language));
+            }
+        }
+
+        private string _textSubject = "random";
+        public string TextSubject {
+            get {
+                return _textSubject;
+            }
+            set {
+                _textSubject = value;
+                OnPropertyChanged(nameof(TextSubject));
             }
         }
     }
