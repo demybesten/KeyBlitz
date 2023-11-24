704371860f0f6da107ff48e9e2665624f7a8d322
Demy437 <demybesten@outlook.com>
2023-11-24 10:51:08 +0100

    gen text

--- Solution/ViewModels/NewTestViewModel.cs
+++ Solution/ViewModels/NewTestViewModel.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using OpenAI.GPT3;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using Solution.Services;
 
 namespace Solution.ViewModels
 {
     public class NewTestViewModel : BaseViewModel{
+
         public NewTestViewModel() {
             _textLength = 20;
             ComplexityLevels.Add("basic");
@@ -21,6 +28,26 @@ namespace Solution.ViewModels
             Languages.Add("dutch");
             Languages.Add("german");
             Languages.Add("french");
+
+
+            SendPromptCommand = new RelayCommand(async () => await SendPrompt(), () => true);
+
+
+        }
+
+        private string _responseText;
+
+        public string ResponseText
+        {
+          get { return _responseText; }
+          set
+          {
+            if (_responseText != value)
+            {
+              _responseText = value;
+              OnPropertyChanged();
+            }
+          }
         }
 
         private int _textLength;
@@ -28,12 +55,12 @@ namespace Solution.ViewModels
             get {
                 return _textLength;
             }
-            set { 
-                _textLength = value; 
+            set {
+                _textLength = value;
                 OnPropertyChanged(nameof(TextLength));
             }
         }
-        
+
         private ObservableCollection<string> _complexityLevels = new ObservableCollection<string>();
         public ObservableCollection<string> ComplexityLevels {
             get {
@@ -65,6 +92,17 @@ namespace Solution.ViewModels
                 _textType = value;
                 OnPropertyChanged(nameof(TextType));
             }
+        }
+
+        private string _complexityLevel;
+        public string ComplexityLevel {
+          get {
+            return _complexityLevel;
+          }
+          set {
+            _complexityLevel = value;
+            OnPropertyChanged(nameof(ComplexityLevel));
+          }
         }
 
         private ObservableCollection<string> _languages = new ObservableCollection<string>();
@@ -99,5 +137,99 @@ namespace Solution.ViewModels
                 OnPropertyChanged(nameof(TextSubject));
             }
         }
+
+         public RelayCommand SendPromptCommand { get; }
+
+
+
+    public async Task SendPrompt()
+    {
+      Dictionary<string, string> complexities = new Dictionary<string, string>
+      {
+        ["basic"] = "basic (no capital letters or punctuation, simple words)",
+        ["average"] = "average (include capital letters and punctuation)",
+        ["advanced"] = "advanced (include capital letters and punctuation, use difficult words)",
+      };
+
+      if (TextSubject == "")
+      {
+        _textSubject = "random";
+      }
+
+      if (TextType.ToUpper() == "WORDS")
+      {
+        _textType = "random words (don't use any capital letters or punctuation)";
+      }else if (TextType.ToUpper() == "SENTENCES")
+      {
+        _textType = "Completely random sentences that don't have any connection, separated by dots. Use capital letters.";
+      }else if (TextType.ToUpper() == "STORY")
+      {
+        foreach (var variable in complexities)
+        {
+          if (variable.Key == ComplexityLevel)
+          {
+            _complexityLevel = variable.Value;
+          }
+        }
+      }
+      string prompt = $"Generate a text for typing practice using the following specifications. DON'T use newlines, ever!\n" +
+                      $"Generate completely random {_textType} \n" +
+                      $"Language: {_language} [EXTREMELY IMPORTANT]\n" +
+                      $"Length: EXACTLY {_textLength} words long.\n" +
+                      $"Subject: {_textSubject}\n" +
+                      $"Text/word complexity: {_complexityLevel}\n\n" +
+                      "PROVIDE THE TEXT IN A SINGLE STRING WITHOUT QUATATION MARKS OR ANY OTHER TEXT (DON'T generate code, use plaintext!)\n" +
+                      "Do NOT go over the specified wordcount!!!\n" +
+                      "DO NOT USE ANY CHARACTERS OUTSIDE THE LATIN ALPHABET, REPLACE FOREIGN LETTERS WITH ALPHANUMERIC CHARACTERS!!!";
+
+
+
+
+      // Declare the API key
+      var apiKey = "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";
+
+// Create an instance of the OpenAIService class
+      var gpt3 = new OpenAIService(new OpenAiOptions()
+        { ApiKey = apiKey });
+
+// Create a chat completion request
+      var completionResult = await gpt3.ChatCompletion.CreateCompletion
+      (new ChatCompletionCreateRequest()
+      {
+        Messages = new List<ChatMessage>(new ChatMessage[]
+          { new ChatMessage("system", prompt) }),
+        Model = "gpt-4-1106-preview",
+        MaxTokens = 100,
+        N = 1
+      });
+
+// Check if the completion result was successful and handle the response
+      StringBuilder resultBuilder = new StringBuilder();
+
+      if (completionResult.Successful)
+      {
+        foreach (var choice in completionResult.Choices)
+        {
+          resultBuilder.AppendLine(choice.Message.Content);
+        }
+      }
+      else
+      {
+        resultBuilder.AppendLine("Error occurred");
+      }
+
+      ResponseText = resultBuilder.ToString();
+
+      // Return the concatenated results as a string
+      Console.WriteLine(resultBuilder.ToString());
+    }
+
+    // INotifyPropertyChanged implementation
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
     }
 }
