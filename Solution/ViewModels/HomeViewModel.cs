4fbdcecddf045bf2d407c9bf288cde48cb7861de
Demy <demybesten@outlook.com>
2023-12-03 17:18:48 +0100

    sync

--- Solution/ViewModels/HomeViewModel.cs
+++ Solution/ViewModels/HomeViewModel.cs
@@ -1,6 +1,28 @@
+using Solution.Helpers;
+using Solution.Services;
+
 namespace Solution.ViewModels;
 
 public class HomeViewModel : BaseViewModel
 {
+    public INavigationService _Navigation;
+
+    public INavigationService Navigation
+    {
+        get => _Navigation;
+        set
+        {
+            _Navigation = value;
+            OnPropertyChanged();
+        }
+    }
     
+    public RelayCommand NavigateToNewTestView { get; set; }
+
+    public HomeViewModel(INavigationService navigation)
+    {
+        Navigation = navigation;
+        NavigateToNewTestView = new RelayCommand(o => { Navigation.NavigateTo<NewTestViewModel>(); }, o => true);
+
+    }
 }
