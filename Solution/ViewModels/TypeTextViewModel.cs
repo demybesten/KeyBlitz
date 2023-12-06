222f1f1fb652b6855ada50ac7b4e373e42178b5e
Demy437 <demybesten@outlook.com>
2023-12-06 13:55:58 +0100

    Live timer

--- Solution/ViewModels/TypeTextViewModel.cs
+++ Solution/ViewModels/TypeTextViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
@@ -14,177 +15,217 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Solution.Helpers;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Solution.ViewModels
 {
-    /*public class FormattedTextLine
+  /*public class FormattedTextLine
+  {
+      public ObservableCollection<Run> TextRuns { get; set; }
+
+      public FormattedTextLine()
+      {
+          TextRuns = new ObservableCollection<Run>();
+      }
+
+      public void AddRun(string text, SolidColorBrush color)
+      {
+          TextRuns.Add(new Run(text) { Foreground = color });
+      }
+  }*/
+
+  public class CharacterEventCommand : ICommand
+  {
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
+
+    public event EventHandler CanExecuteChanged
     {
-        public ObservableCollection<Run> TextRuns { get; set; }
-
-        public FormattedTextLine()
-        {
-            TextRuns = new ObservableCollection<Run>();
-        }
-
-        public void AddRun(string text, SolidColorBrush color)
-        {
-            TextRuns.Add(new Run(text) { Foreground = color });
-        }
-    }*/
-
-    public class CharacterEventCommand : ICommand
-    {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool> _canExecute;
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public CharacterEventCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            _canExecute = canExecute;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
-        }
+      add { CommandManager.RequerySuggested += value; }
+      remove { CommandManager.RequerySuggested -= value; }
     }
 
-    public class TypeTextViewModel : BaseViewModel
+    public CharacterEventCommand(Action<object> execute, Func<object, bool> canExecute = null)
     {
-        private readonly ITextUpdater? _textUpdater;
-        //public ObservableCollection<FormattedTextLine> Lines { get; set; }
+      _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+      _canExecute = canExecute;
+    }
+    public bool CanExecute(object parameter)
+    {
+      return _canExecute == null || _canExecute(parameter);
+    }
 
-        public ICommand MyCommand { get; private set; }
+    public void Execute(object parameter)
+    {
+      _execute(parameter);
+    }
+  }
 
-        public ICommand PressChar { get; private set; }
+  public class TypeTextViewModel : BaseViewModel
+  {
 
-        public ICommand PressBackspace { get; private set; }
+    public DispatcherTimer timer;
+    public Stopwatch stopWatch;
 
-        public TypeTextViewModel()
+    //Slaat stopwatch value op en wordt gebruikt om te binden aan een label
+    private string _elapsedTime = String.Empty;
+    public string ElapsedTime {
+      get {
+        return _elapsedTime;
+      }
+      set {
+        _elapsedTime = value;
+        OnPropertyChanged(nameof(ElapsedTime));
+      }
+    }
+
+    private readonly ITextUpdater? _textUpdater;
+    //public ObservableCollection<FormattedTextLine> Lines { get; set; }
+
+    public ICommand MyCommand { get; private set; }
+
+    public ICommand PressChar { get; private set; }
+
+    public ICommand PressBackspace { get; private set; }
+
+    public TypeTextViewModel()
+    {
+      MyCommand = new RelayCommand(ExecuteMyCommand);
+      PressChar = new CharacterEventCommand(ProcessChar);
+      PressBackspace = new RelayCommand(DeleteCharacter);
+      tempList = new List<int> { };
+      TheText = new List<string> { "Hello", "my", "beautiful", "world!" };
+      UserInput = new List<string> { "" };
+
+      stopWatch = new Stopwatch();
+      timer = new DispatcherTimer();
+      //live timer event
+      timer.Tick += timer_Tick;
+
+      ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
+    }
+
+    private void updateText(List<Word> words)
+    {
+      ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
+      if (_textUpdater != null)
+      {
+        _textUpdater.updateText(words);
+      }
+    }
+
+    public List<string> TheText;
+    public List<string> UserInput;
+    public List<int> tempList;
+
+    private void ProcessChar(object parameter)
+    {
+      stopWatch.Start();
+      timer.Start();
+      /*var text = parameter as string;
+      if (text != null)
+      {
+          tempList.Add(0);
+          updateText(new List<Word>
+          {
+              new Word("temp", tempList),
+          });
+      } else
+      {
+          updateText(new List<Word>
+          {
+              new Word("no text", new List<int> {}),
+          });
+      }*/
+      var text = parameter as string;
+      if (text == " ")
+      {
+        UserInput.Add("");
+      } else if (text?.Length == 1)
+      {
+        UserInput[UserInput.Count - 1] = UserInput[UserInput.Count - 1] + text;
+      }
+      updateInput();
+    }
+
+    private void updateInput()
+    {
+      if (UserInput.Count >= 5) //wanneer de timer stopt
+      {
+        stopWatch.Stop();
+      }
+
+      List<Word> myList = new List<Word> { };
+      for (int w = 0; w < TheText.Count; w++)
+      {
+        string word = TheText[w];
+        string typedWord = "";
+        if (w < UserInput.Count)
         {
-            MyCommand = new RelayCommand(ExecuteMyCommand);
-            PressChar = new CharacterEventCommand(ProcessChar);
-            PressBackspace = new RelayCommand(DeleteCharacter);
-            tempList = new List<int> { };
-            TheText = new List<string> { "Hello", "my", "beautiful", "world!" };
-            UserInput = new List<string> { "" };
-
-            ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
+          typedWord = UserInput[w];
         }
+        List<int> intList = new List<int> { };
 
-        private void updateText(List<Word> words)
+        for (int i = 0; i < word.Length; i++)
         {
-            ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
-            if (_textUpdater != null)
+          //char character = word[i];
+          if (i < typedWord.Length)
+          {
+            if (typedWord[i].Equals(word[i]))
             {
-                _textUpdater.updateText(words);
-            }
-        }
-
-        public List<string> TheText;
-        public List<string> UserInput;
-        public List<int> tempList;
-
-        private void ProcessChar(object parameter)
-        {
-            /*var text = parameter as string;
-            if (text != null)
-            {
-                tempList.Add(0);
-                updateText(new List<Word>
-                {
-                    new Word("temp", tempList),
-                });
+              intList.Add(0);
             } else
             {
-                updateText(new List<Word>
-                {
-                    new Word("no text", new List<int> {}),
-                });
-            }*/
-            var text = parameter as string;
-            if (text == " ")
-            {
-                UserInput.Add("");
-            } else if (text?.Length == 1)
-            {
-                UserInput[UserInput.Count - 1] = UserInput[UserInput.Count - 1] + text;
+              intList.Add(1);
             }
-            updateInput();
+          }
+          else if (w < UserInput.Count - 1)
+          {
+            intList.Add(2);
+          }
         }
 
-        private void updateInput()
+        if (typedWord.Length > word.Length)
         {
-            List<Word> myList = new List<Word> { };
-            for (int w = 0; w < TheText.Count; w++)
-            {
-                string word = TheText[w];
-                string typedWord = "";
-                if (w < UserInput.Count)
-                {
-                    typedWord = UserInput[w];
-                }
-                List<int> intList = new List<int> { };
-
-                for (int i = 0; i < word.Length; i++)
-                {
-                    //char character = word[i];
-                    if (i < typedWord.Length)
-                    {
-                        if (typedWord[i].Equals(word[i]))
-                        {
-                            intList.Add(0);
-                        } else
-                        {
-                            intList.Add(1);
-                        }
-                    }
-                    else if (w < UserInput.Count - 1)
-                    {
-                        intList.Add(2);
-                    }
-                }
-
-                if (typedWord.Length > word.Length)
-                {
-                    for (int i = word.Length; i < typedWord.Length; i++)
-                    {
-                        intList.Add(3);
-                    }
-                    word = word + typedWord.Substring(word.Length, typedWord.Length - word.Length);
-                }
-
-                myList.Add(new Word(word, intList));
-            }
-            updateText(myList);
+          for (int i = word.Length; i < typedWord.Length; i++)
+          {
+            intList.Add(3);
+          }
+          word = word + typedWord.Substring(word.Length, typedWord.Length - word.Length);
         }
 
-        private void ExecuteMyCommand()
-        {
-            ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
-            updateInput();
-        }
-
-        public void DeleteCharacter()
-        {
-            if (UserInput[UserInput.Count-1].Length > 0) {
-                UserInput[UserInput.Count-1] = UserInput[UserInput.Count-1].Substring(0, UserInput[UserInput.Count - 1].Length - 1);
-                updateInput();
-            } else if (UserInput.Count > 1) {
-                UserInput.RemoveAt(UserInput.Count - 1);
-                updateInput();
-            }
-        }
+        myList.Add(new Word(word, intList));
+      }
+      updateText(myList);
     }
+
+    private void ExecuteMyCommand()
+    {
+      ITextUpdater? _textUpdater = ServiceLocator.GetTextUpdater();
+      updateInput();
+    }
+
+    public void DeleteCharacter()
+    {
+      if (UserInput[UserInput.Count-1].Length > 0) {
+        UserInput[UserInput.Count-1] = UserInput[UserInput.Count-1].Substring(0, UserInput[UserInput.Count - 1].Length - 1);
+        updateInput();
+      } else if (UserInput.Count > 1) {
+        UserInput.RemoveAt(UserInput.Count - 1);
+        updateInput();
+      }
+    }
+
+    void timer_Tick(object sender, EventArgs e)
+    {
+      //Als stopwatch runt
+      if (stopWatch.IsRunning)
+      {
+        //Haalt time span op en format deze
+        TimeSpan ts = stopWatch.Elapsed;
+        ElapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+          ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+      }
+    }
+  }
 }
