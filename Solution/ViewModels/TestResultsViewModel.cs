53f6856368e75a93cfaa62a17fd9548ad7129054
Demy <demybesten@outlook.com>
2023-12-11 22:02:59 +0100

    wss nog ff score finetunen maar het werkt

--- Solution/ViewModels/TestResultsViewModel.cs
+++ Solution/ViewModels/TestResultsViewModel.cs
@@ -10,27 +10,28 @@ using Solution.Views;
 namespace Solution.ViewModels
 {
     public class TestResultsViewModel : BaseViewModel
+    
     {
+        private readonly IDataService _dataService;
       public TestResultsViewModel(INavigationService navigation,IDataService passTestStats)
       {
-          
           Navigation = navigation;
           NavigateToScoreView = new NavRelayCommand(o => { Navigation.NavigateTo<ScoreViewModel>(); }, o => true);
           NavigateToTypeTextView = new NavRelayCommand(o => { Navigation.NavigateTo<TypeTextViewModel>(); }, o => true);
           
-        this.passTestStats = passTestStats;
+          _dataService = passTestStats;
         
           // Handle the data change in ViewModelB
-          Wpm = this.passTestStats.Wpm;
-          Cpm = this.passTestStats.Cpm;
-          Score = this.passTestStats.Score;
-          ElapsedTime = this.passTestStats.ElapsedTime;
-          Accuracy = this.passTestStats.Accuracy;
-          AmountOfCorrectChars = this.passTestStats.AmountOfCorrectChars;
-          AmountOfTypedChars = this.passTestStats.AmountOfTypedChars;
-          AmountOfCorrectWords = this.passTestStats.AmountOfCorrectWords;
+
       }
-      
+      public int Wpm => _dataService.Wpm;
+      public int Cpm => _dataService.Cpm;
+      public int Score => _dataService.Score;
+      public string ElapsedTime => _dataService.ElapsedTime;
+      public int Accuracy => _dataService.Accuracy;
+      public double AmountOfCorrectChars => _dataService.AmountOfCorrectChars;
+      public double AmountOfTypedChars => _dataService.AmountOfTypedChars;
+      public double AmountOfCorrectWords => _dataService.AmountOfCorrectWords;
       public INavigationService _Navigation;
 
       public INavigationService Navigation
@@ -45,95 +46,8 @@ namespace Solution.ViewModels
       public NavRelayCommand NavigateToScoreView { get; set; }
       public NavRelayCommand NavigateToTypeTextView { get; set; }
       
-      private readonly IDataService passTestStats;
 
-        private int _wpm;
-        public int Wpm {
-            get {
-                return _wpm;
-            }
-            set {
-                _wpm = value;
-                OnPropertyChanged(nameof(Wpm));
-            }
-        }
 
-        private int _cpm;
-        public int Cpm {
-            get {
-                return _cpm;
-            }
-            set {
-                _cpm = value;
-                OnPropertyChanged(nameof(Cpm));
-            }
-        }
-
-        private string _elapsedTime;
-        public string ElapsedTime {
-            get {
-                return _elapsedTime;
-            }
-            set {
-                _elapsedTime = value;
-                OnPropertyChanged(nameof(ElapsedTime));
-            }
-        }
-
-        private int _score;
-        public int Score {
-            get {
-                return _score;
-            }
-            set {
-                _score = value;
-                OnPropertyChanged(nameof(Score));
-            }
-        }
-        
-        private int _accuracy;
-        public int Accuracy {
-            get {
-                return _accuracy;
-            }
-            set {
-                _accuracy = value;
-                OnPropertyChanged(nameof(Accuracy));
-            }
-        }
-
-        private double _amountOfCorrectChars;
-        public double AmountOfCorrectChars {
-            get {
-                return _amountOfCorrectChars;
-            }
-            set {
-                _amountOfCorrectChars = value;
-                OnPropertyChanged(nameof(AmountOfCorrectChars));
-            }
-        }
-
-        private double _amountOfTypedChars;
-        public double AmountOfTypedChars {
-            get {
-                return _amountOfTypedChars;
-            }
-            set {
-                _amountOfTypedChars = value;
-                OnPropertyChanged(nameof(AmountOfTypedChars));
-            }
-        }
-
-        private double _amountOfCorrectWords;
-        public double AmountOfCorrectWords {
-            get {
-                return _amountOfCorrectWords;
-            }
-            set {
-                _amountOfCorrectWords = value;
-                OnPropertyChanged(nameof(AmountOfCorrectWords));
-            }
-        }
 
 
     }
