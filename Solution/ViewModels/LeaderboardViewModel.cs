7370882a20749f82071d62a439abfed55d7397ac
JoeryDis <127290574+JoeryDis@users.noreply.github.com>
2023-12-11 13:25:03 +0100

    begin leaderboard
    begin leaderboard

--- Solution/ViewModels/LeaderboardViewModel.cs
+++ Solution/ViewModels/LeaderboardViewModel.cs
@@ -1,8 +1,17 @@
 using Solution.Helpers;
 using Solution.Services;
-
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 namespace Solution.ViewModels;
-
+public class Persoon
+{
+    public string Naam { get; set; }
+    public int Score { get; set; }
+}
 public class LeaderboardViewModel : BaseViewModel
 {
     public INavigationService _Navigation;
@@ -16,7 +25,19 @@ public class LeaderboardViewModel : BaseViewModel
             OnPropertyChanged();
         }
     }
+    private ObservableCollection<Persoon> _personen;
 
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public ObservableCollection<Persoon> Personen
+    {
+        get { return _personen; }
+        set
+        {
+            _personen = value;
+            OnPropertyChanged(nameof(Personen));
+        }
+    }
     public NavRelayCommand NavigateToNewTestView { get; set; }
 
     public LeaderboardViewModel(INavigationService navigation)
@@ -24,5 +45,30 @@ public class LeaderboardViewModel : BaseViewModel
         Navigation = navigation;
         NavigateToNewTestView = new NavRelayCommand(o => { Navigation.NavigateTo<NewTestViewModel>(); }, o => true);
 
+    }
+    public LeaderboardViewModel()
+    {
+        // Initialisatie van de personenlijst (voorbeeldgegevens)
+        Personen = new ObservableCollection<Persoon>
+        {
+            new Persoon { Naam = "Alice", Score = 90 },
+            new Persoon { Naam = "Bob", Score = 75 },
+            new Persoon { Naam = "Charlie", Score = 85 },
+            new Persoon { Naam = "David", Score = 95 },
+            new Persoon { Naam = "Eva", Score = 80 }
+        };
+
+        // Uitvoeren van de LINQ-query
+        var hoogScorendePersonen = from persoon in Personen
+                                   where persoon.Score > 80
+                                   orderby persoon.Score descending
+                                   select persoon;
+
+        Personen = new ObservableCollection<Persoon>(hoogScorendePersonen);
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- Solution/Views/LeaderboardView.xaml.cs
+++ Solution/Views/LeaderboardView.xaml.cs
@@ -1,11 +1,21 @@
+using Solution.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Solution.Views;
 
 public partial class LeaderboardView : UserControl
 {
+    public LeaderboardViewModel ViewModel { get; set; }
+
     public LeaderboardView()
     {
         InitializeComponent();
+        ViewModel = new LeaderboardViewModel();
+        DataContext = ViewModel;
+
     }
+
+ 
+
 }
