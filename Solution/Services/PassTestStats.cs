4f69eefe3943a8aa47467f1b29177314fdefcf55
Demy437 <demybesten@outlook.com>
2023-12-13 11:25:53 +0100

    Revert "Merge pull request #21 from demybesten/HerhalenTekst"
    This reverts commit 030a75800885617cdeecd60a735322eb1561105e, reversing
    changes made to 0ebb5670cfc68de84bc5adf4ac673d8922e70654.

--- Solution/Services/PassTestStats.cs
+++ Solution/Services/PassTestStats.cs
@@ -1,128 +1,20 @@
 ﻿using System;
-using System.ComponentModel;
-using Solution.Helpers;
+
 namespace Solution.Services;
 
-public class PassTestStats : IDataService,INotifyPropertyChanged
+public class PassTestStats : IDataService
 {
-private double _amountOfCorrectChars;
-    public double AmountOfCorrectChars
-    {
-        get { return _amountOfCorrectChars; }
-        set
-        {
-            if (_amountOfCorrectChars != value)
-            {
-                _amountOfCorrectChars = value;
-                OnPropertyChanged(nameof(AmountOfCorrectChars));
-            }
-        }
-    }
+  public double AmountOfCorrectChars { get; set; }
+  public double AmountOfTypedChars { get; set; }
+  public double AmountOfCorrectWords { get; set; }
+  public int Score { get; set; }
 
-    private double _amountOfTypedChars;
-    public double AmountOfTypedChars
-    {
-        get { return _amountOfTypedChars; }
-        set
-        {
-            if (_amountOfTypedChars != value)
-            {
-                _amountOfTypedChars = value;
-                OnPropertyChanged(nameof(AmountOfTypedChars));
-            }
-        }
-    }
+  public int Wpm { get; set; }
 
-    private double _amountOfTypedWords;
-    public double AmountOfTypedWords
-    {
-        get { return _amountOfTypedWords; }
-        set
-        {
-            if (_amountOfTypedWords != value)
-            {
-              _amountOfTypedWords = value;
-                OnPropertyChanged(nameof(AmountOfTypedWords));
-            }
-        }
-    }
+  public int Cpm { get; set; }
 
-    private int _score;
-    public int Score
-    {
-        get { return _score; }
-        set
-        {
-            if (_score != value)
-            {
-                _score = value;
-                OnPropertyChanged(nameof(Score));
-            }
-        }
-    }
+  public int Accuracy { get; set; }
 
-    private int _wpm;
-    public int Wpm
-    {
-        get { return _wpm; }
-        set
-        {
-            if (_wpm != value)
-            {
-                _wpm = value;
-                OnPropertyChanged(nameof(Wpm));
-            }
-        }
-    }
-
-    private int _cpm;
-    public int Cpm
-    {
-        get { return _cpm; }
-        set
-        {
-            if (_cpm != value)
-            {
-                _cpm = value;
-                OnPropertyChanged(nameof(Cpm));
-            }
-        }
-    }
-
-    private int _accuracy;
-    public int Accuracy
-    {
-        get { return _accuracy; }
-        set
-        {
-            if (_accuracy != value)
-            {
-                _accuracy = value;
-                OnPropertyChanged(nameof(Accuracy));
-            }
-        }
-    }
-
-    private string _elapsedTime;
-    public string ElapsedTime
-    {
-        get { return _elapsedTime; }
-        set
-        {
-            if (_elapsedTime != value)
-            {
-                _elapsedTime = value;
-                OnPropertyChanged(nameof(ElapsedTime));
-            }
-        }
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    protected virtual void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
   public string ElapsedTime { get; set; }
 
   public string? Text { get; set; }
